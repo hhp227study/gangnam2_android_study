4f80b5e7ac42fdb06ff9e77831686f44a95b6414
hhp227 <hong227@naver.com>
2025-12-30 15:58:07 +0900

    fix: CI 에러 나는 부분 수정(Firebase 부분 비활성화)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,10 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    id("com.google.gms.google-services")
+}
+
+if (!project.hasProperty("firebase.disable")) {
+    apply(plugin = "com.google.gms.google-services")
 }
 
 android {
