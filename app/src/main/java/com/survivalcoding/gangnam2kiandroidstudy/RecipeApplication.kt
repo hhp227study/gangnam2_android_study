aff3f1124564741d61a96096c197413b8817a658
hhp227 <hong227@naver.com>
2026-01-02 18:36:13 +0900

    feat: Firestore 에뮬레이터 환경 추가

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeApplication.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/RecipeApplication.kt
@@ -1,6 +1,8 @@
 package com.survivalcoding.gangnam2kiandroidstudy
 
 import android.app.Application
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.authModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.clipboardModule
 import com.survivalcoding.gangnam2kiandroidstudy.core.di.daoModule
@@ -30,6 +32,16 @@ class RecipeApplication : Application() {
                 databaseModule,
                 daoModule,
                 authModule
+            )
+        }
+        if (BuildConfig.FLAVOR == "staging") {
+            FirebaseFirestore.getInstance().useEmulator(
+                "10.0.2.2",
+                8080
+            )
+            FirebaseAuth.getInstance().useEmulator(
+                "10.0.2.2",
+                9099
             )
         }
     }
